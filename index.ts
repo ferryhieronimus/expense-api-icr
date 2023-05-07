fbd914117c621d6553f413d735e23bb7bf917a6e
Hieronimus Ferry <ferryhieronimus@gmail.com>
2023-05-08 04:06:03 +0700

    Add morgan

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 import * as middlewares from './middlewares';
 import * as routes from './routes';
 
 const app: express.Application = express();
+
 app.use(express.json());
+app.use(morgan("tiny"));
 app.use(routes.expenseRouter)
 app.use(middlewares.zodErrorHandler);
 
