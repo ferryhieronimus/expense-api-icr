552fe963ff32b86f1e148926844b55ac9c1f749d
Hieronimus Ferry <ferryhieronimus@gmail.com>
2023-05-08 11:40:03 +0700

    Add swagger

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import morgan from "morgan";
 import * as middlewares from './middlewares';
 import * as routes from './routes';
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json";
 
 const app: express.Application = express();
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(routes.expenseRouter)
